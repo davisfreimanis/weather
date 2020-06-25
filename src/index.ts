a9a49b2e8802db44ec85d8cff3d3ea192c183ee6
Davis Freimanis <davis@freiman.is>
2020-06-25 19:01:21 +0200

    Fetch weather from SMHI API

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import fetch from "node-fetch";
 
-console.log("hello world!");
+console.log("hello world!!");
 
-const url =
-  "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16/lat/58/data.json";
-fetch(url)
-  .then((res) => res.json())
-  .then((json) => console.log(json));
+if (navigator.geolocation) {
+  // Get position using Geolocation API
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude.toFixed(6);
+    const long = position.coords.longitude.toFixed(6);
+    console.log(`${lat}, ${long}`);
+
+    const url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${long}/lat/${lat}/data.json`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        const { timeSeries } = json;
+        console.log(timeSeries);
+      });
+  });
+}
